29741eb25472c3e218fe47d704ff859830f5cb6d
OverCat2000 <malishavimukthi@gmail.com>
2024-05-12 05:32:55 +0530

    changes

--- Backend/App.py
+++ Backend/App.py
@@ -1,9 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
-from sklearn.pipeline import Pipeline
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import OneHotEncoder
 import joblib
 
 df = pd.read_csv("online_shoppers_intention_cleaned.csv")
